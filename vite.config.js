3d84cbb5c8ef38972765d9f3ab511747138b1281
mungu <jmge164@naver.com>
2026-02-15 14:17:07 +0900

    2026-02-15 design add message-log add

--- vite.config.js
+++ vite.config.js
@@ -15,30 +15,30 @@ export default defineConfig({
     host: '0.0.0.0', // 모든 IP로부터의 접속을 허용
     port: 5173,      // 포트 번호 확인
     https: {
-      key: fs.readFileSync('../certs/key.pem'),
-      cert: fs.readFileSync('../certs/cert.pem'),
+      // key: fs.readFileSync('../certs/key.pem'),
+      // cert: fs.readFileSync('../certs/cert.pem'),
     },
     proxy: {
-      '/login': {
-        target: 'https://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false, // Self-signed cert support
-      },
-      '/casualties': {
-        target: 'https://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/insa': {
-        target: 'https://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/generate-report': {
-        target: 'https://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,
-      },
+      // '/login': {
+      //   target: 'https://127.0.0.1:8000',
+      //   changeOrigin: true,
+      //   secure: false, // Self-signed cert support
+      // },
+      // '/casualties': {
+      //   target: 'https://127.0.0.1:8000',
+      //   changeOrigin: true,
+      //   secure: false,
+      // },
+      // '/insa': {
+      //   target: 'https://127.0.0.1:8000',
+      //   changeOrigin: true,
+      //   secure: false,
+      // },
+      // '/generate-report': {
+      //   target: 'https://127.0.0.1:8000',
+      //   changeOrigin: true,
+      //   secure: false,
+      // },
     }
   }
 })
